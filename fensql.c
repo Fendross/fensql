aa30b3c6877487981a8dffd2a339f57b651bfa67
Fendross <dario.arzuffi@icloud.com>
2025-04-27 14:20:36 +0200

    Added .gitignore and implemented InputBuffer struct.

--- fensql.c
+++ fensql.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+/*
+A simple sqlite clone.
+Steps to implement it:
+    - Coding a REPL (read-execute-print loop) --> IN PROGRESS
+*/
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() { printf("fensql > "); }
+
+
+// Main fensql program.
 int main(int argc, char* argv[]) {
-    printf("hello fensql\n");
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer); // TODO implement this.
+
+        if () {
+            close_input_buffer(); // TODO implement this
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
 }

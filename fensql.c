fe28915f782cf84fd4f0a1f91a52be78b87f8080
Fendross <dario.arzuffi@icloud.com>
2025-04-27 21:22:31 +0200

    Implemented Row, Table, related constants and functionalities.

--- fensql.c
+++ fensql.c
@@ -6,20 +6,81 @@
 #include "input_buffer.h"
 
 /*
-A simple sqlite clone.
+Fensql - A simple sqlite clone.
 
 Generic Notes:
     - Meta commands are non-SQL statements that, in our case, start with a dot, for example .exit
     - The SQL compiler parses a string and outputs an internal representation (bytecode), allowing for:
         - Reduced complexity for each part (virtual machine doesn't have to check for syntax errors)
         - Compiling of common queries once and caching the produced bytecode for improved performance
-    - TODO
+    - Step 3 limits to a hard-coded table that looks like this:
+        id            integer
+        username      varchar(32)
+        email         varchar(255)
+    - An example of an insert statement would be: insert 1 fendross foo@bar.com
+    - First approach for step 3 will be to not use a B-tree but an array to represent a table
+    - Block of memory will be called pages, and the approach is the following:
+        Store rows in blocks of memory called pages
+        Each page stores as many rows as it can fit
+        Rows are serialized into a compact representation with each page
+        Pages are only allocated as needed
+        Keep a fixed-size array of pointers to pages
 
 Steps to implement it:
-    - Coding a REPL (read-execute-print loop)   --> DONE
-    - Adding more Keywords (insert, select)     --> DONE
-    - In-Memory, Append-only, Single-table DB   --> IN PROGRESS
+    1. Coding a REPL (read-execute-print loop)   --> DONE
+    2. Adding more Keywords (insert, select)     --> DONE
+    3. In-Memory, Append-only, Single-table DB   --> IN PROGRESS
 */
+
+// Defines and constants.
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+#define TABLE_MAX_PAGES 100
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t PAGE_SIZE = 4096; // 4 KB, same size as used in virtual memory systems in most computer architectures.
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE; // 291 bytes.
+
+
+// Row struct and functionalities.
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+
+// Table struct and functionalities.
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
 
 // Meta Command enum and functionalities.
 typedef enum {
@@ -27,7 +88,7 @@ typedef enum {
     META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+MetaCommandResult execute_meta_command(InputBuffer* input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         exit(EXIT_SUCCESS);
     } else {
@@ -43,10 +104,12 @@ typedef enum {
 
 typedef struct {
     StatementType type;
+    Row row_to_insert; // Only used by insert statement.
 } Statement;
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -54,6 +117,19 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     // Using strncmp since the "insert" keyword will not be followed by data.
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+
+        // Parse argument.
+        int args_assigned = sscanf(
+            input_buffer->buffer,
+            "insert %d %s %s",
+            &(statement->row_to_insert.id),
+            &(statement->row_to_insert.username),
+            &(statement->row_to_insert.email),
+        );
+        if (args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
+
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
@@ -117,7 +193,7 @@ int main(int argc, char* argv[]) {
 
         // Handle any meta command.
         if (input_buffer->buffer[0] == '.') {
-            switch (do_meta_command(input_buffer)) {
+            switch (execute_meta_command(input_buffer)) {
                 case (META_COMMAND_SUCCESS):
                     continue;
                 case (META_COMMAND_UNRECOGNIZED_COMMAND):
@@ -126,10 +202,14 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        // Handle SQL statements.
         Statement statement;
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error detected for '%s'.\n", input_buffer->buffer);
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at the start of '%s'.\n", input_buffer->buffer);
                 continue;
